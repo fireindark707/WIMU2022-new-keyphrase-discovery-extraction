0ca2206d6226e431236867e429ed201b9fb0e1e5
Staler2019 <abcd121234@yahoo.com.tw>
2022-06-11 12:19:45 +0800

    Choose v7 to run

--- method-comparison/keybert-2020/run_keyBert.py
+++ method-comparison/keybert-2020/run_keyBert.py
@@ -10,7 +10,7 @@ STOPWORD_FILE = "../spacy_stopwords/zh.txt"
 TEST_FILE = f"../data/{FILE_TYPE}.csv"
 DICT_FILE = f"../data/tagged/{FILE_TYPE}_dict.txt"
 OUT_FILE_PARENT = "./predict"
-OUT_FILE = f"{OUT_FILE_PARENT}/{FILE_TYPE}_add_dict_v4_.csv"
+OUT_FILE = f"{OUT_FILE_PARENT}/{FILE_TYPE}_v8_modify_model_with_jieba_use_cut_for_search.csv"
 
 reg = re.compile("[/\n]")
 
@@ -28,7 +28,7 @@ def get_my_dict(dict_loc):
 
 if __name__ == '__main__':
     stopwords = get_stopwords(STOPWORD_FILE)
-    kb = KeyBERT() # default model: all-MiniLM-L6-v2
+    kb = KeyBERT(model="paraphrase-multilingual-MiniLM-L12-v2") # default model: all-MiniLM-L6-v2
     # jieba initials
     for my_word in get_my_dict(DICT_FILE):
         jieba.add_word(my_word)
@@ -47,6 +47,7 @@ if __name__ == '__main__':
 
         # ketBert: take keyword after tokenization
         text_tokens = jieba.cut(text)
+        # text_tokens = jieba.cut_for_search(text)
         # text_tokens = set(list(jieba.cut_for_search(text)))
 
         # for token1 in text_tokens.copy():
@@ -57,8 +58,23 @@ if __name__ == '__main__':
         #             text_tokens.remove(token2)
 
         text = " ".join(list(text_tokens))
-        keywords = kb.extract_keywords(text, stop_words=stopwords, top_n=10, )
+        keywords = kb.extract_keywords(text, stop_words=stopwords, top_n=10, diversity=0.2, use_mmr=True)
+        # keywords = kb.extract_keywords(text, stop_words=stopwords, top_n=50, )
 
+        # keywords.sort()
+        # for k1 in keywords.copy():
+        #     for k2 in keywords.copy():
+        #         if len(k1[0]) <= len(k2[0]):
+        #             continue
+        #         elif k2[0] in k1[0]:
+        #             keywords.remove(k2)
+
+        # new_keywords = []
+        # for keyword in keywords:
+        #     new_keywords.append((keyword[1], keyword[0]))
+        # new_keywords.sort(reverse=True)
+
+        # predict["keywords"].append([keyword[1] for keyword in new_keywords[:10]])
         predict["keywords"].append([keyword[0] for keyword in keywords])
 
         succ_counter += 1
